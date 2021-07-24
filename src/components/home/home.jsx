02c660828d40a4e958ba982052603611651ef77f
ausi1997.com <ausafen1018@gmail.com>
2021-07-25 02:33:16 +0530

    updated

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -2,12 +2,11 @@ import React, { useEffect, useState } from 'react';
 
 const Home = ()=>{
  
-    const [data,setData] = useState([]);
+
 
     useEffect(()=>{
         localStorage.clear();
-        setData(localStorage);
-    },[data]);
+     });
     return(
         <div>
         home
